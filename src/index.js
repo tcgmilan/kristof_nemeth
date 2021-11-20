303b2bef8635e6a8309633ed10ddaad3551a46a7
Horváth Balázs <habszi024@gmail.com>
2021-11-20 18:11:54 +0100

    basic files without any changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+const Routing = () =>{
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
